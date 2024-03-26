0e15ba1aabe42473c9d005c5d9641651eee2032b
Alex Greenberg <acgreenberg2@gmail.com>
2024-03-26 14:01:19 -0400

    1-4 complete for nfl

--- nfl.sql
+++ nfl.sql
@@ -1,14 +1,16 @@
 -- 1. List the names of all NFL teams'
+SELECT * FROM teams
 
 
 -- 2. List the stadium name and head coach of all NFC teams
-
+SELECT stadium, head_coach FROM teams
 
 -- 3. List the head coaches of the AFC South
-
+SELECT head_coach FROM teams
+    WHERE division = 'South' AND conference = 'AFC'
 
 -- 4. The total number of players in the NFL
-
+SELECT COUNT(*) FROM players AS player_count
 
 -- 5. The team names and head coaches of the NFC North and AFC East
 
